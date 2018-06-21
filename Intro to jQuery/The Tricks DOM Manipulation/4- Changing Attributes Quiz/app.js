cfdbf07b5a371ed5a7d39e287ea599428c266c89
Fatima Shihab <fatima.shihab@hotmail.com>
2018-06-21 08:16:43 +0300

    submit tasks of lesson 2: tricks of dom manipulation

---
+++ Intro to jQuery/The Tricks DOM Manipulation/4- Changing Attributes Quiz/app.js
@@ -0,0 +1,13 @@
+/*
+For this quiz, set the href of the <a> in the first nav item to "#1".
+
+You must use jQuery's attr() method!
+*/
+
+// Start with this variable!
+var navList;
+
+navList = $('.nav-list');
+link=navList.first()
+link.find('a').attr('href',"#1");
+// your code goes here!
---
+++ Intro to jQuery/The Tricks DOM Manipulation/8- Removing DOM Elements/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, remove the <ul> from the first article item!
+
+You must use jQuery's remove() method.
+*/
+
+// Start with this variable!
+var articleItems;
+
+$('.article-item').children('ul').remove();// your code goes here!
